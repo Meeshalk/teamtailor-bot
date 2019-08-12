372ed3e1e33ae4c4b5cc4461488bae2ed1b00f5d
meeshal k <meeshal1994@gmail.com>
2019-08-13 04:18:49 +0530

    final submit

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,4 +1,4 @@
-<a href="index.html" class="logo">
+<a href="{{ route('home') }}" class="logo">
   <!-- mini logo for sidebar mini 50x50 pixels -->
   <span class="logo-mini"><b>T</b>C</span>
   <!-- logo for regular state and mobile devices -->
