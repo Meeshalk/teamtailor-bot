1ea5d64d26a5891683a46b795be5d5ae2057dcb4
meeshal k <meeshal1994@gmail.com>
2019-08-14 01:33:41 +0530

    version correction - stable release

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="pull-right hidden-xs">
-    <b>Version</b> 0.0.2
+    <b>Version</b> 0.1.2 - stable
   </div>
   <strong>Copyright &copy; 2019 <a href="https://digiseva.co.in">Digiseva Pvt Ltd</a>.</strong> All rights
   reserved.
